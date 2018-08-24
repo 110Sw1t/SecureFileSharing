b3124a0f38fb9600265d4f38897840da33ba475a
farida-abouish <32582370+farida-abouish@users.noreply.github.com>
2018-08-24 21:27:59 +0200

    AES
    AES implementation

---
+++ src/main/java/AES.java
@@ -0,0 +1,43 @@
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Random;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ *
+ * @author Farida Abouish
+ */
+public class AES {
+
+    public SecretKey keyGenerate() throws NoSuchAlgorithmException {
+        KeyGenerator key = KeyGenerator.getInstance("AES");
+        key.init(128);
+        SecretKey finalKey = key.generateKey();
+        return finalKey;
+    }
+
+    public static byte[] encrypt(SecretKey key, String plainText) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException {
+        Cipher cipher = Cipher.getInstance("AES");
+        SecretKeySpec secretKey = new SecretKeySpec(key.getEncoded(), "AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] cipherText = cipher.doFinal(plainText.getBytes("UTF8"));
+        return cipherText;
+    }
+
+    public static byte[] decrypt(SecretKey key, byte[] cipherText) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        SecretKeySpec secretKey = new SecretKeySpec(key.getEncoded(), "AES");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] plainText = cipher.doFinal(cipherText);
+        return plainText;
+    }
+
+}
